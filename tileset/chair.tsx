7df83c3b72887c07879c38da960a21b61c4c07e0
pascal zwick <pascal.zwick@quentic.io>
2023-11-15 10:25:30 +0100

    new chair on new map

---
+++ tileset/chair.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="2023.11.13" name="chair" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../../../Users/pascal.zwick/Downloads/chair.png" width="256" height="256"/>
+</tileset>
